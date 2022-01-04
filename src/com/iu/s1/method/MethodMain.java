51b2418c76f0d632db6074261533dd6514638ae1
yoohaemin <yoohm0630@gmail.com>
2022-01-04 14:34:49 +0900

    make project

---
+++ src/com/iu/s1/method/MethodMain.java
@@ -0,0 +1,12 @@
+package com.iu.s1.method;
+
+public class MethodMain {
+
+	public static void main(String[] args) {
+		System.out.println("MainMethod Start");
+		
+		System.out.println("MainMethod Finish");
+
+	}
+
+}
