fb05366d5f645cae07d4cb75de9c4baabbdbd7fe
yoohaemin <yoohm0630@gmail.com>
2022-01-04 16:34:32 +0900

    add st

---
+++ src/com/iu/s1/stuent/StudnetMain.java
@@ -0,0 +1,16 @@
+package com.iu.s1.stuent;
+
+import java.util.Scanner;
+
+public class StudnetMain {
+
+	public static void main(String[] args) {
+		// makeStudnet 호출
+		// 이름, 번호, 국어, 영어, 수학 출력
+		
+		
+		
+
+	}
+
+}
---
+++ src/com/iu/s1/stuent/Stuent.java
@@ -0,0 +1,14 @@
+package com.iu.s1.stuent;
+
+public class Stuent {
+	
+	String neme;
+	int num;
+	int kor;
+	int eng;
+	int math;
+	
+	int total;
+	double avg;
+	
+}
